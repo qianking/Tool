49e081297aecb7128b36c59fd876200ff6f8c5d5
qianking <a98041652@gmail.com>
2022-07-25 00:05:28 +0800

    pdf

--- pdf_merge/write_word.py
+++ pdf_merge/write_word.py
@@ -3,6 +3,17 @@ from docxtpl import DocxTemplate
 doc_template_path = r'E:\python\github\Tool\pdf_merge\封面\template.docx'
 doc_output_path = r'E:\python\github\Tool\pdf_merge\封面\test_1.docx'
 doc = DocxTemplate(doc_template_path)
-context = {'number' : "V534"}
+context = {
+        
+        "data": [
+
+            {'title' : "1-1~1-10設計概要說明"},
+            {'title' : "1-11重量計算"}
+
+
+        ]                 
+            
+    }
+
 doc.render(context)
 doc.save(doc_output_path)
