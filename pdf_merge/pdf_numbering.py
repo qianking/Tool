bc7aa46661c2bc8f720f55dc9a5c0268c2165047
Qianking <a98041652@gmail.com>
2022-07-25 18:26:44 +0800

    pdf_merge

--- pdf_merge/pdf_numbering.py
+++ pdf_merge/pdf_numbering.py
@@ -5,20 +5,16 @@ from PyPDF2 import PdfFileReader, PdfFileWriter, PdfMerger
 from glob import glob
 from copy import deepcopy
 import re
+import write_word
 
 """
 如果要使用本檔案，需先去 python\lib\site-packages\PyPDF2\_camp.py 檔案中的第287行 註解掉這行
 """
 
-path = r'D:\download\整合PDF\整合前\test_1.pdf'
-outputFile = r'C:\Users\andy_chien\Downloads\整合PDF\Test_2.pdf'
-path_home=r'D:\download\整合PDF\整合前\02_V534_(111.04.13)_地震風力整合大全(100年)_110.12.31_Locked.pdf'
 
+self = ''
+status = ''
 
-
-folder = r'D:\download\整合PDF-1\整合前'
-file_list = glob(f"{folder}\*.pdf")
-#print(file_list)
 
 
 class Add_Page_Number():
@@ -79,24 +75,27 @@ class Merge_Pdf_and_GetOutline():
     def __init__(self, folder_path):
         self.folder_path = folder_path
         self.file_list = glob(f"{self.folder_path}\*.pdf")
+        self.debug_file_list = []
+        self.debug_title_list = []
         self.order_dic = {1:{0:['結構資料', None]}, 2:{0:['地震力與風力計算',None]}, 3:{0:['結構設計檢核', '.\\封面\\第三章封面.pdf']}, 4:{0:['開挖設計', '.\\封面\\第四章封面.pdf']}, 5:{0:['結構外審意見回覆', '.\\封面\\第五章封面.pdf']}, 6:{0:['設計分析報表', '.\\封面\\第六章封面.pdf']}}
         self.title = {1:['結構資料', '1-1~1-10．設計概要說明', '1-11．建築物重量計算', '1-12．動力分析週期', '1-13．振態說明', '1-14．剛性隔板質心及剛心'],
                     2:['地震力與風力計算', '2-1．建築物設計地震力計算', '2-2．垂直地震力計算', '2-3．建築物地震力之豎向分配', '2-4．動力反應譜分析調整放大係數', '2.5．動力分析樓層剪力', '2.6．動力分析質心位移', '2.7．動力分析層間變位角', '2.8．意外扭矩放大係數計算', '2-9．碰撞間隔及層間變位角計算', '2-10．風力計算'], 
                     3:['結構設計檢核', '軟層檢核', '剪力牆設計', '一樓樓版剪力傳遞', '梁上柱檢核', '梁柱韌性與扭力檢核', '極限層剪力檢核', '上浮力檢核', '地下室外牆設計', '無梁版檢核', '基礎設計'],
-                    4:['開挖設計', '開挖設計'],
+                    4:['開挖設計'],
                     5:['結構外審意見回覆', '意見回覆'],
                     6:['設計分析報表','大梁', '小梁']}
         
-        self.insert_page = {1:None,
+        ''' self.insert_page = {1:None,
                             2:None,
                             3:'.\\封面\\第三章封面.pdf',
                             4:'.\\封面\\第四章封面.pdf',
                             5:'.\\封面\\第五章封面.pdf',
-                            6:'.\\封面\\第六章封面.pdf'}
+                            6:'.\\封面\\第六章封面.pdf'} '''
         
         self.outline = {1:{}, 2:{}, 3:{}, 4:{}, 5:{}, 6:{}}
+        self.to_word_outline = {}
 
-    def merge_pdf(self):
+    def merge_and_getpage(self):
         page = 0
         merger = PdfMerger(strict = False)
         for key, values in self.order_dic.items():
@@ -118,8 +117,8 @@ class Merge_Pdf_and_GetOutline():
         
     def order_file(self):
         patern = r'(\d\d)_(\d\d)'
-        
-        for pdf in file_list:
+        self.debug_file_list = deepcopy(self.file_list)
+        for pdf in self.file_list:
             file_name = pdf.split('\\')[-1]
             num_list = re.findall(patern, file_name)
             chapter = int(num_list[0][0])
@@ -129,6 +128,9 @@ class Merge_Pdf_and_GetOutline():
             for title_name in self.title[chapter]:
                 if title_name in file_name:
                     self.order_dic[chapter][num] =[title_name, pdf] 
+                    index = self.debug_file_list.index(pdf)
+                    self.debug_file_list.pop(index)                          #找到符合的pdf就pop出來，直到找完
+            
             
         for i in list(self.order_dic.keys()):
             self.order_dic[i] = dict(sorted(self.order_dic[i].items()))
@@ -136,38 +138,27 @@ class Merge_Pdf_and_GetOutline():
     
     def get_outline_page(self, key, title_name, page, PdfReader):
         if key == 1:
-            self.title_1_list = list(deepcopy(self.title[1][1:])) 
+            self.title_1_2_list = list(deepcopy(self.title[1][1:]))
+            self.title_1_2_list.extend(list(deepcopy(self.title[2][1:])))
+            self.debug_title_list = deepcopy(self.title_1_2_list)
             self.outline[1][self.title[1][0]] = None
+            self.outline[2][self.title[2][0]] = None 
             _page = PdfReader.getNumPages()
-            for pages in range(_page):                      #跑片第一大章節裡面所有 在self.title[1][1:]之中 的小標題 
-                if len(self.title_1_list) != 0:
-                    title = self.title_1_list[0]
-                    pure_title = title.split('．')[1]
-                else:
-                    tmp_page = pages                             #沒有小標題時就挑出迴圈，並且儲存跑到第幾頁                
-                    break
+
+            for pages in range(_page):                       
                 Page_n = PdfReader.getPage(pages)
                 txt = Page_n.extractText()
-                
-                if pure_title in txt:
-                    title = title.replace('．', ' ')
-                    self.outline[1][title] = pages+1
-                    self.title_1_list.pop(0)                     #每找到一個小標題就pop出來，直到沒有
-
-
-            self.outline[2][self.title[2][0]] = None               #跑片第二大章節裡面所有 在self.title[2][1:]之中 的小標題 
-            self.title_2_list = list(deepcopy(self.title[2][1:])) 
-            for pages in range(tmp_page, _page):
-                if len(self.title_2_list) != 0:
-                    title = self.title_2_list[0]
+                if len(self.debug_title_list) != 0:
+                    title = self.debug_title_list[0]
                     pure_title = title.split('．')[1]
-                Page_n = PdfReader.getPage(pages)
-                txt = Page_n.extractText()
-                
-                if title in txt:
-                    title = title.replace('．', ' ')
-                    self.outline[2][title] = pages+1
-                    self.title_2_list.pop(0)
+                    try:
+                        title_num = int(title.split('-')[0])
+                    except:
+                        title_num = int(title.split('.')[0])
+                    if pure_title in txt:
+                        title_change = title.replace('．', ' ')
+                        self.outline[title_num][title_change] = pages + 1
+                        self.debug_title_list.pop(0)                   
          
         elif key >= 3:
             _page = PdfReader.getNumPages()
@@ -180,13 +171,91 @@ class Merge_Pdf_and_GetOutline():
 
         return page
 
+    def transfer_outline(self):
+        for chapter, data in self.outline.items():
+            tmp_list = []
+            title_list = list(data.keys())
+            for title in title_list:
+                if data[title_list[0]]:
+                    self.to_word_outline[f'cover_{chapter}'] = deepcopy(data[title_list[0]])
+                for tit in title_list[1:]:
+                    tmp_dic = {}
+                    tmp_dic['title'] = deepcopy(tit)
+                    tmp_dic['page'] = deepcopy(data[tit])
+                    tmp_list.append(deepcopy(tmp_dic))
+                    tmp_dic.clear()
+                self.to_word_outline[f'title_{chapter}'] = deepcopy(tmp_list)
+                del tmp_list[:]
+
+def send_msg_to_UI(msg):
+    if self != '':
+        self.status.emit(msg)
+
+
+def get_variable_form_UI(**args):
+    global self
+    global status
+    if 'self' in args:
+        self = args['self']
+        status = args['status']
+        return args['number'], args['address'], args['name'], args['folder']
+
+
+def check_folder_file(folder_path):
+    illegal_file_list = []
+    patern = r'\d\d_\d\d'
+    file_list = glob(f"{folder_path}\*.pdf")
+    pdf_file_list = [i.split('\\')[-1] for i in file_list]
+    for file in pdf_file_list:
+        if_num = re.findall(patern, file)
+        if len(if_num) == 0:
+            illegal_file_list.append(file)
+    if len(illegal_file_list) != 0:
+        return illegal_file_list
+
+
+
+def get_first_page_and_merge_pdf(number, address, name, folder_path):
+    try:
+        pdf = Merge_Pdf_and_GetOutline(folder_path)
+        pdf.order_file()
+        print('debug_file_list:', pdf.debug_file_list)
+        pdf.merge_and_getpage()
+        print('debug_title_list', pdf.debug_title_list)
+        pdf.transfer_outline()
+        word_outline = pdf.to_word_outline
+        word_outline['number'] = number
+        word_outline['address'] = address
+        word_outline['name'] = name
+    except Exception as ex:
+        pass
+
+    else:
+        write_word.write_word(folder_path, word_outline)
+
+
+def main(**args):
+    number, address, name, folder_path= get_variable_form_UI(**args)
+    illegal_file_list = check_folder_file(folder_path)
+    if illegal_file_list:
+        msg = f'find illegal file in {folder_path} : \n{illegal_file_list}'
+        send_msg_to_UI(msg)
+        return 0
+    get_first_page_and_merge_pdf(number, address, name, folder_path)
+
+        
+
+        
+    
+
+
+
+            
+
 
 if "__main__" == __name__:
     #print(u"\u0121")
-    pdf =  Merge_Pdf_and_GetOutline(folder)
-    pdf.order_file()
-    print(pdf.order_dic)
-    pdf.merge_pdf()
-    print(pdf.outline) 
+    check_folder_file('C:\\Users\\andy_chien\\Downloads\\整合PDF(all)\\整合前')
+    
     
     
--- pdf_merge/write_word.py
+++ pdf_merge/write_word.py
@@ -1,19 +1,60 @@
 from docxtpl import DocxTemplate
+import datetime
+import os
 
-doc_template_path = r'E:\python\github\Tool\pdf_merge\封面\template.docx'
-doc_output_path = r'E:\python\github\Tool\pdf_merge\封面\test_1.docx'
-doc = DocxTemplate(doc_template_path)
-context = {
-        
-        "data": [
+doc_template_path = r'\封面\template.docx'
+doc_output_path = r'C:\封面\test_1.docx'
+''' context = {
+        'title_1': [
+            {'title': '1-1~1-10 設計概要說明', 'page': 1}, 
+            {'title': '1-11 建築物重量計算', 'page': 7}, 
+            {'title': '1-12 動力分析週期', 'page': 11}, 
+            {'title': '1-13 振態說明', 'page': 13}, 
+            {'title': '1-14 剛性隔板質心及剛心', 'page': 15}
+            ], 
+        'title_2': [
+            {'title': '2-1 建築物設計地震力計算', 'page': 16}, 
+            {'title': '2-2 垂直地震力計算', 'page': 18}, 
+            {'title': '2-3 建築物地震力之豎向分配', 'page': 19}, 
+            {'title': '2-4 動力反應譜分析調整放大係數', 'page': 23}, 
+            {'title': '2.5 動力分析樓層剪力', 'page': 24}, 
+            {'title': '2.6 動力分析質心位移', 'page': 27}, 
+            {'title': '2.7 動力分析層間變位角', 'page': 28}, 
+            {'title': '2.8 意外扭矩放大係數計算', 'page': 31}, 
+            {'title': '2-9 碰撞間隔及層間變位角計算', 'page': 32}, 
+            {'title': '2-10 風力計算', 'page': 34}], 
+        'cover_3': 44, 
+        'title_3': [
+            {'title': '軟層檢核', 'page': 45}, 
+            {'title': '剪力牆設計', 'page': 47}, 
+            {'title': '一樓樓版剪力傳遞', 'page': 53}, 
+            {'title': '梁上柱檢核', 'page': 57}, 
+            {'title': '梁柱韌性與扭力檢核', 'page': 59}, 
+            {'title': '極限層剪力檢核', 'page': 69}, 
+            {'title': '上浮力檢核', 'page': 71}, 
+            {'title': '地下室外牆設計', 'page': 72}, 
+            {'title': '無梁版檢核', 'page': 75}, 
+            {'title': '基礎設計', 'page': 93}], 
+        'cover_4': 99, 
+        'title_4': [], 
+        'cover_5': 117, 
+        'title_5': [
+            {'title': '意見回覆', 'page': 118}
+            ], 
+        'cover_6': 130, 
+        'title_6': [
+            {'title': '大梁', 'page': 131}
+            ]
+        } '''
 
-            {'title' : "1-1~1-10設計概要說明"},
-            {'title' : "1-11重量計算"}
+def output_path(folder_path):
+    now_date = datetime.date.today()
+    file_name = f'{now_date}_frist_page.docx'
+    doc_output_path = os.path.join(folder_path, file_name)
+    return doc_output_path
 
-
-        ]                 
-            
-    }
-
-doc.render(context)
-doc.save(doc_output_path)
+def write_word(folder_path, context):
+    doc_output_path = output_path(folder_path)
+    doc = DocxTemplate(doc_template_path)
+    doc.render(context)
+    doc.save(doc_output_path)
