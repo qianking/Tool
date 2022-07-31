8a919d278d1328f17e76d6bbf2586b5c268225c5
qianking <a98041652@gmail.com>
2022-08-01 00:19:37 +0800

    pdf

--- pdf_merge/pdf_merger_2.py
+++ pdf_merge/pdf_merger_2.py
@@ -1,3 +1,4 @@
+from xml.dom import NotFoundErr
 from reportlab.pdfgen import canvas
 import os
 from PyPDF2 import PdfFileReader, PdfFileWriter, PdfMerger
@@ -11,118 +12,265 @@ import write_word_pdf as word_pdf
 如果要使用本檔案，需先去 python\lib\site-packages\PyPDF2\_camp.py 檔案中的第287行 註解掉這行
 """
 
-All_chapter = \
-{1:'結構資料',
-2:'地震力與風力計算',
-3:'結構設計檢核',
-4:'開挖設計',    #開挖設計這章沒小章節，所以頁數不用+1
-5:'結構外審意見回覆',
-6:'設計分析報表'
-}
-All_file_name = \
-{1:['結構資料'],
-3: ['軟層檢核', '剪力牆設計', '一樓樓版剪力傳遞', '梁上柱檢核', '梁柱韌性與扭力檢核', '極限層剪力檢核', '上浮力檢核', '地下室外牆設計', '無梁版檢核', '基礎設計'],
-4: ['開挖設計'],
-5: ['第一次意見回覆','第二次意見回覆', '第三次意見回覆', '會後意見回覆'],
-6: ['大梁、柱、牆', '小梁、版']}
+All_same_chapter = \
+{1: {'title': '結構設計檢核', 'inner_title_and_file_name' : ['軟層檢核', '剪力牆設計', '一樓樓版剪力傳遞', '梁上柱檢核', '梁柱韌性與扭力檢核', '極限層剪力檢核', 'SRC梁檢核', 'SRC柱檢核', '上浮力檢核', '地下室外牆設計', '無梁版檢核', '基礎設計']},
+2: {'title': '開挖設計', 'inner_title_and_file_name' : None}, #開挖設計這章沒小章節，所以頁數不用+1
+3: {'title': '結構外審意見回覆', 'inner_title_and_file_name' : ['第一次意見回覆','第二次意見回覆', '第三次意見回覆', '會後意見回覆']},
+4: {'title': '設計分析報表', 'inner_title_and_file_name' : ['大梁、柱、牆', '小梁、版']}}
 
-Chapter_1_inner_title = \
-{1:['1-1~1-10．設計概要說明', '1-11．建築物重量計算', '1-12．動力分析週期', '1-13．振態說明', '1-14．剛性隔板質心及剛心'],
-2: ['2-1．建築物設計地震力計算', '2-2．垂直地震力計算', '2-3．建築物地震力之豎向分配', '2-4．動力反應譜分析調整放大係數', '2.5．動力分析樓層剪力', '2.6．動力分析質心位移', '2.7．動力分析層間變位角', '2.8．意外扭矩放大係數計算', '2-9．碰撞間隔及層間變位角計算', '2-10．風力計算']}
+
+#核章版
+Stamp_ver_Chapter_1_2_data = \
+{0: {'file_name': '結構資料'},
+1:{'title': '結構設計概要說明', 'inner_title' : ['1-1．建築概要','1-2．結構系統', '1-3．結構模型示意圖', '1-4．設計規範', '1-5．主要材料強度', '1-6．設計載重', '1-7．構材尺寸', '1-8．分析程式', '1-9．載重組合', '1-10．地震作用時層間變位檢討', '1-11．建築物重量計算', '1-12．動力分析週期', '1-13．振態說明', '1-14．剛性隔板質心及剛心']},
+2: {'title': '地震力與風力計算', 'inner_title' :['2-1．建築物設計地震力計算', '2-2．垂直地震力計算', '2-3．建築物地震力之豎向分配', '2-4．動力反應譜分析調整放大係數', '2.5．動力分析樓層剪力', '2.6．動力分析質心位移', '2.7．動力分析層間變位角', '2.8．意外扭矩放大係數計算', '2-9．碰撞間隔及層間變位角計算', '2-10．風力計算']}}
+
+#外審版
+Audit_ver_Chapter_1_inner_title = \
+{0: {'file_name': '外審意見回覆'}, 1:{'title': '外審意見回覆', 'inner_title' :['附件n']}}
+
 
 Chapter_number = \
-{1: '一',
-2:'二',
-3:'三',
-4:'四',
-5:'五',
-6:'六'}
+{1: '一', 2:'二', 3:'三', 4:'四', 5:'五', 6:'六'}
+
+stytle = 'Stamp'
+
 
 class Merge_Pdf_and_GetOutline():
     def __init__(self, input_pdf_folder_path, output_path):
         self.input_pdf_folder_path = input_pdf_folder_path
         self.output_path = output_path
         self.file_list = glob(f"{self.input_pdf_folder_path}\*.pdf")
+        self.special_chapter_file_path = ''
+        self.same_chapter_flag = None
+        self.all_chapter_dic = {}
+        self.to_word_outline = {}
+        
 
-    def order_file(self):
-        order_dic = {1:{1:{'結構資料':[]}}, 3:{}, 4:{}, 5:{}, 6:{}}
-        self.debug_file_list = deepcopy(self.file_list)
+    def create_order_dic(self):
+        self.same_chapter_dic = {}
+        len_order = len(Chapter_number)
+        for i in range(1, len_order+1):
+            self.same_chapter_dic[i] = {}
+
+        self.special_chapter_dic = {1:[]}
+
+
+    def find_same_chapter_file(self):
         for pdf in self.file_list:
             pdf_name = pdf.split('\\')[-1]
-            order_dic = self.find_file_chapter(order_dic, pdf_name)
-        
-        self.sort_order_dic(order_dic)
+            self.find_the_same_chapter(pdf_name)
 
-        #print(order_dic)
-        self.final_order_file = self.re_order_chapter_number(order_dic)
-        print(self.final_order_file)
 
-    @staticmethod
-    def find_file_chapter(order_dic, pdf_name):
-        tmp_dic = {}
-        for chapter, file_name_list in All_file_name.items():
-            for name in file_name_list:
+    def find_the_same_chapter(self, pdf_name):
+        for chapter, file_name_list in All_same_chapter.items():
+            if file_name_list['inner_title_and_file_name']:
+                for name in file_name_list['inner_title_and_file_name']:
+                    if name in pdf_name:
+                        name_index = file_name_list['inner_title_and_file_name'].index(name)
+                        pdf_path = os.path.join(self.input_pdf_folder_path, pdf_name)
+                        self.same_chapter_dic[chapter][name_index+1] = {'title': name, 'pdf_path': pdf_path}
+                        self.same_chapter_dic[chapter][0] = {'title': file_name_list['title']}
+                        return 0
+            else:
+                name = file_name_list['title']
                 if name in pdf_name:
-                    if chapter == 1:
-                        order_dic[1][1]['結構資料'].append(pdf_name)
+                    pdf_path = os.path.join(self.input_pdf_folder_path, pdf_name)
+                    self.same_chapter_dic[chapter][1] = {'title': None, 'pdf_path': pdf_path}
+                    self.same_chapter_dic[chapter][0] = {'title': name}
+
+
+    def find_special_chapter_file(self):
+        flag = False
+        if stytle == 'Stamp':
+            for pdf in self.file_list:
+                pdf_name = pdf.split('\\')[-1]
+                if Stamp_ver_Chapter_1_2_data[0]['file_name'] in pdf_name:
+                    self.special_chapter_dic[1].append(pdf)
+                    flag = True
+                    break
+            
+            if not flag:
+                raise FileNotFoundError('找不到核章版版第一章檔案')
+                  
+        if stytle == 'Audit':
+            for pdf in self.file_list:
+                pdf_name = pdf.split('\\')[-1]
+                if Audit_ver_Chapter_1_inner_title[0]['file_name'] in pdf_name:
+                    self.special_chapter_dic[1].append(pdf)
+                    flag = True
+                    break
+
+            if not flag:
+                raise FileNotFoundError('找不到外審版第一章檔案')
+                
+    
+    def find_Stamp_page(self):
+        self.special_chapter_file_path = deepcopy(self.special_chapter_dic[1][0])
+        self.special_chapter_dic.clear()
+        for i in range(1,3):
+            self.special_chapter_dic[i] = {}
+            self.special_chapter_dic[i][0] = {'title' : Stamp_ver_Chapter_1_2_data[i]['title'], 'page' : None}
+            
+        title_1_2_list = list(deepcopy(Stamp_ver_Chapter_1_2_data[1]['inner_title']))
+        title_1_2_list.extend(list(deepcopy(Stamp_ver_Chapter_1_2_data[2]['inner_title'])))
+
+        PdfReader = PdfFileReader(self.special_chapter_file_path)
+        total_page = PdfReader.getNumPages()
+        self.special_chapter_dic['total_page'] =total_page
+        
+        for pages in range(total_page):
+            Page_n = PdfReader.getPage(pages)
+            txt = Page_n.extractText()
+            while len(title_1_2_list) != 0:
+                title = title_1_2_list[0]
+                title_name = title.split('．')[1]
+                pattern = re.compile(fr"(\d).(\d\d*)．({title_name})", re.I)
+                find_pattern = pattern.findall(txt)
+                if len(find_pattern) != 0:
+                    chapter = int(find_pattern[0][0])
+                    name = f"{find_pattern[0][0]}-{find_pattern[0][1]} {find_pattern[0][2]}"
+                    self.special_chapter_dic[chapter][int(find_pattern[0][1])] = {'title': name, 'page': pages + 1}
+                    index_title = title_1_2_list.index(title)
+                    title_1_2_list.pop(index_title)
+                else:
+                    break
+
+        if len(title_1_2_list) != 0:
+            raise NotFoundErr(f'{self.special_chapter_file_path}中，章節{title_1_2_list[0]}未被找到，請檢查檔案')
+
+
+    def find_special_chapter_page(self):
+        if stytle == 'Stamp':
+            self.find_Stamp_page()
+        if stytle == 'Audit':
+            pass
+            
+
+    def order_same_chpater(self):
+        count_ch = 1
+        for capter in range(1, len(self.same_chapter_dic)+1):
+            title_count = 0
+            count = 0
+            tmp_dic = {}
+            if len(self.same_chapter_dic[capter]) != 0:
+                title_data = self.same_chapter_dic.pop(capter)
+                while len(title_data) != 0:
+                    try:
+                        tmp_dic[count] = title_data.pop(title_count)
+                    except:
+                        title_count +=1
                     else:
-                        order_dic[chapter][0] = deepcopy(All_chapter[chapter])
-                        index = file_name_list.index(name)
-                        tmp_dic[name] = pdf_name
-                        order_dic[chapter][index+1] = deepcopy(tmp_dic)
-                        tmp_dic.clear()
-        return order_dic
+                        count += 1
+                self.same_chapter_dic[count_ch] = deepcopy(tmp_dic)
+                count_ch +=1
+            else:
+                self.same_chapter_dic.pop(capter)
     
-    @staticmethod
-    def sort_order_dic(order_dic):
-        for num in list(order_dic.keys()):
-            if len(order_dic[num]) == 0:
-                order_dic.pop(num)
-                continue
-            order_dic[num] = dict(sorted(order_dic[num].items()))
-    
-    @staticmethod
-    def re_order_chapter_number(order_dic):
-        start_num = 3
+                
+    def add_same_and_special_chapter(self):
+        now_pages = self.special_chapter_dic.pop('total_page')
+        chapter_count = 1
+        for i in range(1, len(self.special_chapter_dic)+1):
+            self.all_chapter_dic[chapter_count] = self.special_chapter_dic.pop(i)
+            chapter_count += 1
+
+        self.same_chapter_flag = chapter_count
+        now_pages+=1
+        self.get_same_chapter_page_cover_pdg(chapter_count, now_pages)
+
+        for chapter in list(self.same_chapter_dic.keys()):
+            self.all_chapter_dic[chapter_count] = self.same_chapter_dic.pop(chapter)
+            chapter_count += 1
+            
+
+    def get_same_chapter_page_cover_pdg(self, chapter_count, now_pages):
+        for chapter, data in self.same_chapter_dic.items():
+            for key, values in data.items():
+                if key == 0:
+                    cover_chapter = Chapter_number[chapter_count]
+                    cover_name =  self.same_chapter_dic[chapter][0]['title']
+                    pdf_output_path = self.generate_cover_pdf(cover_chapter, cover_name)
+                    self.same_chapter_dic[chapter][0]['pdf_path'] = pdf_output_path
+
+                PdfReader = PdfFileReader(values['pdf_path'])
+                get_page = PdfReader.getNumPages()
+                self.same_chapter_dic[chapter][key]['page'] = now_pages 
+                now_pages += get_page
+
+            chapter_count+= 1
+
+
+    def generate_cover_pdf(self, chapter, name):
         tmp_dic = {}
-        chapter_1 = order_dic.pop(1)
-        tmp_dic[1] = chapter_1
-        for chapter in list(order_dic.keys()):
-            chapter_n = order_dic.pop(chapter)
-            tmp_dic[start_num] = chapter_n
-            start_num += 1
-        return tmp_dic
+        tmp_dic['chapter'] = chapter
+        tmp_dic['title'] = name
+        doc_output_path = word_pdf.write_cover_word(self.output_path, f'cover_{chapter}', tmp_dic)
+        pdf_output_path = word_pdf.turn_word_to_pdf(doc_output_path)
+        return pdf_output_path 
+
+    def get_output_file_path(self):
+        now_date = datetime.date.today()
+        file_name = f'{now_date}_merge.pdf'
+        self.output_merge_pdf_path = os.path.join(self.output_path, file_name)    
+
+
+    def merge_all_pdf(self):
+        merger = PdfMerger()
+        merger.append(self.special_chapter_file_path)
+        for chapter in range(self.same_chapter_flag, len(self.all_chapter_dic)+1):
+            for key, value in self.all_chapter_dic[chapter].items():
+                merger.append(value.pop('pdf_path'))
+        
+
+        self.get_output_file_path()
+        merger.write(self.output_merge_pdf_path)
+        merger.close()
+
     
-    def generate_cover_pdf(self):
-        tmp_dic = {}
-        chapter_list = list(self.final_order_file.keys())
-        for chpater in chapter_list[1:]:
-            chapter_name = self.final_order_file[chpater][0]
-            chapter_num = Chapter_number[chpater]
-            tmp_dic['chapter'] = chapter_num
-            tmp_dic['title'] = chapter_name
-            doc_output_path = word_pdf.write_cover_word(self.output_path, f'cover_{chpater}', tmp_dic)
-            pdf_output_path = word_pdf.turn_word_to_pdf(doc_output_path)
-            self.final_order_file[chpater][0] = {chapter_name : pdf_output_path}
-    
+    def transfer_to_word_stytle(self):
+        temp_list = []
+        for chapter in range(1, len(self.all_chapter_dic)+1):
+            temp_dic = {}
+            temp_inner_list = []
+            temp_dic['big_title'] = f"{Chapter_number[chapter]}、{self.all_chapter_dic[chapter][0]['title']}"
+            if chapter >= self.same_chapter_flag:
+                temp_dic['page'] = self.all_chapter_dic[chapter][0]['page']
+            for inner_chapter in range(1, len(self.all_chapter_dic[chapter])):
+                if not self.all_chapter_dic[chapter][inner_chapter]['title']:
+                    temp_inner_list = []
+                else:
+                    temp_inner_list.append(deepcopy(self.all_chapter_dic[chapter][inner_chapter]))
+            
+            temp_dic['inner_title'] = deepcopy(temp_inner_list)
+            temp_list.append(deepcopy(temp_dic))
 
-    def one_build_set(self):
-        
-        
-
-
-        
-
-             
-    
-        
-
+            if chapter == self.same_chapter_flag-1:
+                self.to_word_outline['title_special'] = deepcopy(temp_list)
+                del temp_list[:]
+            if chapter == len(self.all_chapter_dic):
+                self.to_word_outline['title_same'] = deepcopy(temp_list)
+                del temp_list[:]
+            
 
 
 if "__main__" == __name__:
-    pdf = Merge_Pdf_and_GetOutline(r'E:\python\github\Tool\pdf_merge\整合PDF(all)\整合前', r'E:\python\github\Tool\pdf_merge\整合PDF(all)\整合前')
-    pdf.order_file()
-    pdf.generate_cover_pdf()
-    print(pdf.final_order_file)
+    pdf = Merge_Pdf_and_GetOutline(r'E:\python\github\Tool\pdf_merge\整合PDF(all)\整合前', r'E:\python\github\Tool\pdf_merge\整合PDF(all)\整合前\merge_file')
+    pdf.create_order_dic()
+    pdf.find_same_chapter_file()
+    pdf.order_same_chpater()
+
+    pdf.find_special_chapter_file()
+    pdf.find_special_chapter_page()
+    pdf.add_same_and_special_chapter()
+    print(pdf.same_chapter_dic)
+    print(pdf.special_chapter_dic)
+    #print(pdf.special_chapter_file_path)
+    pdf.merge_all_pdf()
+    #print(pdf.all_chapter_dic)
+    pdf.transfer_to_word_stytle()
+    print(pdf.to_word_outline)
+    
+    
 
 
--- pdf_merge/write_word_pdf.py
+++ pdf_merge/write_word_pdf.py
@@ -3,52 +3,50 @@ import datetime
 import os
 import comtypes.client
 
-outline_template_path = r'E:\python\github\Tool\pdf_merge\整合PDF(all)\cover\outline_template_2.docx'
+outline_template_2_path = r'E:\python\github\Tool\pdf_merge\整合PDF(all)\cover\outline_template_2.docx'
+outline_template_path = r'E:\python\github\Tool\pdf_merge\整合PDF(all)\cover\outline_template.docx'
 cover_template_path = r'.\cover\cover_template.docx'
 #doc_output_path = r'C:\封面\test_1.docx'
-""" context = {
-        'title_1': [
-            {'title': '1-1~1-10 設計概要說明', 'page': 1}, 
-            {'title': '1-11 建築物重量計算', 'page': 7}, 
-            {'title': '1-12 動力分析週期', 'page': 11}, 
-            {'title': '1-13 振態說明', 'page': 13}, 
-            {'title': '1-14 剛性隔板質心及剛心', 'page': 15}
-            ], 
-        'title_2': [
-            {'title': '2-1 建築物設計地震力計算', 'page': 16}, 
-            {'title': '2-2 垂直地震力計算', 'page': 18}, 
-            {'title': '2-3 建築物地震力之豎向分配', 'page': 19}, 
-            {'title': '2-4 動力反應譜分析調整放大係數', 'page': 23}, 
-            {'title': '2.5 動力分析樓層剪力', 'page': 24}, 
-            {'title': '2.6 動力分析質心位移', 'page': 27}, 
-            {'title': '2.7 動力分析層間變位角', 'page': 28}, 
-            {'title': '2.8 意外扭矩放大係數計算', 'page': 31}, 
-            {'title': '2-9 碰撞間隔及層間變位角計算', 'page': 32}, 
-            {'title': '2-10 風力計算', 'page': 34}, 
-            ],
-        'cover_3': 44, 
-        'title_3': [
-            {'title': '軟層檢核', 'page': 45}, 
-            {'title': '剪力牆設計', 'page': 47}, 
-            {'title': '一樓樓版剪力傳遞', 'page': 53}, 
-            {'title': '梁上柱檢核', 'page': 57}, 
-            {'title': '梁柱韌性與扭力檢核', 'page': 59}, 
-            {'title': '極限層剪力檢核', 'page': 69}, 
-            {'title': '上浮力檢核', 'page': 71}, 
-            {'title': '地下室外牆設計', 'page': 72}, 
-            {'title': '無梁版檢核', 'page': 75}, 
-            {'title': '基礎設計', 'page': 93}], 
-        'cover_4': 99, 
-        'title_4': [], 
-        'cover_5': 117, 
-        'title_5': [
-            {'title': '意見回覆', 'page': 118}
-            ], 
-        'cover_6': 130, 
-        'title_6': [
-            {'title': '大梁', 'page': 131}
-            ]
-        } """
+context = {
+        'title_special': [
+            {'big_title':'一、結構設計概要說明', 
+                'inner_title': [
+                    {'title': '1-1~1-10 設計概要說明', 'page': 1}, 
+                    {'title': '1-11 建築物重量計算', 'page': 7}, 
+                    {'title': '1-12 動力分析週期', 'page': 11}, 
+                    {'title': '1-13 振態說明', 'page': 13}, 
+                    {'title': '1-14 剛性隔板質心及剛心', 'page': 15}
+                ]}, 
+            {'big_title': '二、地震力與風力計算', 
+                'inner_title': [
+                {'title': '2-1 建築物設計地震力計算', 'page': 16}, 
+                {'title': '2-2 垂直地震力計算', 'page': 18}, 
+                {'title': '2-3 建築物地震力之豎向分配', 'page': 19}, 
+                {'title': '2-4 動力反應譜分析調整放大係數', 'page': 23}, 
+                {'title': '2.5 動力分析樓層剪力', 'page': 24}, 
+                {'title': '2.6 動力分析質心位移', 'page': 27}, 
+                {'title': '2.7 動力分析層間變位角', 'page': 28}, 
+                {'title': '2.8 意外扭矩放大係數計算', 'page': 31}, 
+                {'title': '2-9 碰撞間隔及層間變位角計算', 'page': 32}, 
+                {'title': '2-10 風力計算', 'page': 34}, 
+                ]}],
+        'title_same':[
+            {'big_title':'三、結構設計檢核', 'page': 44,
+                'inner_title': [
+                    {'title': '軟層檢核', 'page': 45}, 
+                    {'title': '剪力牆設計', 'page': 47}, 
+                    {'title': '一樓樓版剪力傳遞', 'page': 53}, 
+                    {'title': '梁上柱檢核', 'page': 57}, 
+                    {'title': '梁柱韌性與扭力檢核', 'page': 59}, 
+                    {'title': '極限層剪力檢核', 'page': 69}, 
+                    {'title': '上浮力檢核', 'page': 71}, 
+                    {'title': '地下室外牆設計', 'page': 72}, 
+                    {'title': '無梁版檢核', 'page': 75}, 
+                    {'title': '基礎設計', 'page': 93}
+                ]},
+            {'big_title':'四、開挖設計', 'page': 99, 
+                'inner_title': []}], 
+        }
 
 context_cover = {'title_1_1' : '1-1~1-10設計概要說',
                 'data' : [
@@ -93,8 +91,8 @@ def output_path(output_folder_path, file_name):
     doc_output_path = os.path.join(output_folder_path, out_put_file_name)
     return doc_output_path
 
-def write_word(folder_path, context):
-    doc_output_path = output_path(folder_path)
+def write_word(folder_path, file_name, context):
+    doc_output_path = output_path(folder_path, file_name)
     doc = DocxTemplate(outline_template_path)
     doc.render(context)
     doc.save(doc_output_path)
@@ -122,4 +120,4 @@ def turn_word_to_pdf(input_word):
 
 
 if "__main__" == __name__:
-    write_word(r'E:\python\github\Tool\pdf_merge\整合PDF(all)\cover', context_cover_2)
+    write_word(r'E:\python\github\Tool\pdf_merge\整合PDF(all)\cover', 'test', context)
