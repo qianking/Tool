8e39d3f725bc742df13179fc3021372627b5dc52
qianking <a98041652@gmail.com>
2022-09-12 23:08:47 +0800

    Designdata

---
+++ DesignData/Main.py
@@ -0,0 +1,110 @@
+import os
+import sys
+import random
+import traceback
+
+input_path = r'E:\python\github\Tool\hhh\設計報表.txt'
+output_path = r'E:\python\github\Tool\hhh\test.txt'
+
+ui_signal = None
+
+def send_to_ui_status(txt):
+    ui_signal.status.emit(txt)
+
+def data_from_ui(signals):
+    global ui_signal
+    ui_signal = signals
+
+
+def debug(func):
+    def warpper(*args, **wargs):
+        try:
+            output_path = func(*args, **wargs)
+        except Exception as ex:
+            error_class = ex.__class__.__name__ #取得錯誤類型
+            detail = ex.args[0] #取得詳細內容
+            cl, exc, tb = sys.exc_info() #取得Call Stack
+            lastCallStack = traceback.extract_tb(tb)[-1] #取得Call Stack的最後一筆資料
+            fileName = lastCallStack[0] #取得發生的檔案名稱
+            lineNum = lastCallStack[1] #取得發生的行號
+            funcName = lastCallStack[2]#取得發生的函數名稱
+            errMsg = f"{[error_class]}\n\"{fileName}\", line {lineNum}, in {funcName}\n{detail}"
+            print(errMsg)
+            send_to_ui_status(f'Error: {errMsg}')
+        else:
+            send_to_ui_status('轉換完成')
+            send_to_ui_status(f'OUTPUT PATH: {output_path}')
+    return warpper
+
+
+def transfer(input_path):
+    replace_name = ('Beam/Column capacity ratio exceeds limit', 
+                    'Shear stress due to shear force and torsion together exceeds maximum allowed',
+                    'Reinforcing required exceeds maximum allowed', 
+                    'Joint shear ratio exceeds limit',
+                    'Shear stress exceeds maximum allowed')
+
+    with open(input_path, 'r') as f:
+        data = f.read()
+    #print(repr(data))
+    
+    data_list = data.split('\n')
+    error_index_dic = dict()
+    continue_flag = False
+    for i, dd in enumerate(data_list):
+        continue_flag = False
+        for name in replace_name:
+            if name in dd:
+                continue_flag = True
+                error_index_dic[i] = dd.replace(name, '').rstrip()
+                break
+            
+        if (not continue_flag) and len(error_index_dic):
+            random_data = find_same_len(data_list[list(error_index_dic.keys())[0]-10: list(error_index_dic.keys())[0]], len(error_index_dic))
+            for i, j in enumerate(error_index_dic):
+                ddd = error_index_dic[j] + random_data[i][len(error_index_dic[j]):]
+                data_list[j] = ddd
+            error_index_dic.clear()
+    
+    full_data = '\n'.join(data_list)
+
+
+    output_path = fr"{input_path.split('.')[0]}OUTPUT.txt"
+    with open(output_path, 'w+') as f:
+        f.write(full_data)
+    return output_path
+
+
+def find_same_len(data_n_list, random_num):
+    data_random_list = list()
+    len_list = [len(i.rstrip()) for i in data_n_list]
+    len_set = tuple(set(len_list))
+    len_dic = {len_list.count(len_data) : len_data for len_data in len_set}
+    max_len_count = max(list(len_dic.keys()))
+    max_len = len_dic[max_len_count]
+    for i in data_n_list:
+        if len(i.rstrip()) == max_len:
+            data_random_list.append(i)
+    
+    random_data = random.sample(data_random_list, random_num)
+    return random_data
+
+
+@debug
+def Transfer_Flow(input_path, signals=None):
+    data_from_ui(signals)
+    output_path = transfer(input_path)
+    return output_path
+
+
+
+
+
+
+
+
+
+
+
+if "__main__" == __name__:
+    Transfer_Flow(input_path)
---
+++ DesignData/designData_ui_controller.py
@@ -0,0 +1,122 @@
+import sys
+from PySide6 import QtCore, QtWidgets
+from PySide6.QtCore import QFile, QThread, Signal, Qt, QRunnable, QThreadPool, QObject
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication, QFileDialog, QMainWindow
+import Main as transfer
+from designdata_ui import Ui_MainWindow
+
+UI_file_format = 'py'
+VERSION = '0.01'
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__()
+        if UI_file_format == 'py':
+            self._window = Ui_MainWindow()
+            self._window.setupUi(self)
+        elif UI_file_format == 'ui':
+            self._window = None  
+
+        self.threadpool = QThreadPool()
+        self.threadpool.setMaxThreadCount(1)     
+        self.setup_ui()
+
+    @property
+    def window(self):
+        return self._window
+
+    def setup_ui(self):
+        if UI_file_format == 'ui':
+            loader = QUiLoader()
+            file = QFile('./DesignData_ui.ui')
+            file.open(QFile.ReadOnly)
+            self._window = loader.load(file)
+            file.close()
+        self.set_window_title()
+        self.Input_btm()
+        self.filename_lineedit()
+        self.plain_txt()
+        
+    
+    def set_window_title(self):
+        if UI_file_format == 'ui':
+            self._window.setWindowTitle(f'DesignData V {VERSION}')    #.ui版本
+        else:
+            self.setWindowTitle(f'DesignData V {VERSION}')             #.py版本
+    
+    def Input_btm(self):
+        self.Input = self._window.input_btm
+        self.Input.clicked.connect(self.open_file)
+        self.Input.clicked.connect(self.start_thread)
+
+    def filename_lineedit(self):
+        self.showfilename = self._window.lineEdit 
+    
+    def open_file(self):
+        self.show_information.clear()
+        self.input_path, filetype = QFileDialog.getOpenFileName(self, 'Open folder', 'F:/', "txt (*txt)")
+        print(self.input_path)
+        self.input_path = self.input_path.replace('/', '\\')
+        self.showfilename.setText(self.input_path)
+
+    def start_thread(self):
+        self.start_thread = start_process(self.input_path)
+        self.start_thread.signals.status.connect(self.send_to_status)
+        self.threadpool.start(self.start_thread)
+    
+    def plain_txt(self):
+        self.show_information = self._window.plainTextEdit
+
+    def send_to_status(self, txt):
+        fft1 = self.show_information.currentCharFormat()
+        if "WORNING" in txt or "Error" in txt:
+            fft1.setForeground(Qt.red)
+            self.show_information.setCurrentCharFormat(fft1)
+            self.show_information.insertPlainText(f"{txt}\n")
+        
+        elif '轉換完成' in txt: 
+            fft1.setForeground(Qt.blue)
+            self.show_information.setCurrentCharFormat(fft1)
+            self.show_information.insertPlainText(f"{txt}\n")
+
+        elif ': ' in txt:
+            txt_list = txt.split(': ')
+            fft1.setForeground(Qt.darkGreen)
+            self.show_information.setCurrentCharFormat(fft1)
+            self.show_information.insertPlainText(f"{txt_list[0]}: ")
+            fft1.setForeground(Qt.black)
+            self.show_information.setCurrentCharFormat(fft1)
+            self.show_information.insertPlainText(f"{txt_list[1]}\n")
+
+
+class thread_signal(QObject):
+    status = Signal(str)
+
+class start_process(QRunnable):
+    def __init__(self, input_path):
+        super(start_process, self).__init__()  
+        self.signals = thread_signal()
+        self.input_path = input_path
+        
+    def run(self):
+        transfer.Transfer_Flow(self.input_path, self.signals)
+
+    
+if '__main__' == __name__:
+    #QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
+    qt_app = QtWidgets.QApplication(sys.argv)
+    
+    app = QApplication.instance()
+    if app is None:
+        app = QApplication(sys.argv)
+
+    mainwindow_1 = MainWindow()
+    if UI_file_format == 'ui':
+        mainwindow_1.window.show()  #.ui版本
+    else:
+        mainwindow_1.show()          #.py版本
+
+    sys.exit(app.exec())
+
