1cfbf218afe0f2ed8f7eb8120eac99b4493f8c2c
qianking <a98041652@gmail.com>
2022-10-24 01:54:38 +0800

    gemini

--- gemini/test/test_4.py
+++ gemini/test/test_4.py
@@ -1,6 +1,20 @@
 from test_3 import SingleTonNew
+from functools import wraps
 
 
+
+class Fail_Dealer():
+    def __init__(self):
+        self.jjj = SingleTonNew()
+
+    def __call__(self, func):
+        @wraps(func)
+        def decorated(*args, **kwargs):
+            func(*args, **kwargs)
+            print('no')
+            print(self.jjj.i.a)
+
+        return decorated
 
 class jj():
     
@@ -8,7 +22,8 @@ class jj():
 
     def __init__(self):
         pass
-        
+    
+    @Fail_Dealer()
     def add(self, b):
         self.pp.i.a += b
         print(self.pp.i.a)
