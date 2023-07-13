10f8bc6f9d54c32218a1b911556dd87e81e3e065
Andy_Chien <andy_chien@pegatroncorp.com>
2023-07-13 17:46:02 +0800

    LSFresult 2023/07/13

--- LFSResult/code/LSF_Main.py
+++ LFSResult/code/LSF_Main.py
@@ -1,9 +1,14 @@
+import sys
+import os
+import traceback
+import time
+from enum import Enum
+from PySide6.QtCore import Qt
 from LSF_DataTransfer import DataTransfer
 from LSF_ToExcel import Transfer_Excel
 from LSF_ToPDF import Transfer_PDF, PDF_Merge
 
-root_path = r"C:\Users\andy_chien\Downloads\資料\弱層檢核\OUTPUT"
-
+root_path = r'C:\Users\andy_chien\Downloads\資料\弱層檢核\OUTPUT'
 data = {'input_X_shear':rf'{root_path}\V534VPDATXE_NSW.txt',
         'input_X':rf'{root_path}\V534VPDATXE.txt',
         'input_Y_shear':rf'{root_path}\V534VPDATYE_NSW.txt',
@@ -12,21 +17,76 @@ data = {'input_X_shear':rf'{root_path}\V534VPDATXE_NSW.txt',
         'level':'1MF',
         'H':5.45,}
 
-excel_output_path = r'C:\Users\andy_chien\Downloads\資料\弱層檢核\OUTPUT\report.xlsx'
-pdf_output_file = r'C:\Users\andy_chien\Downloads\資料\弱層檢核\OUTPUT'
-final_pdf = r"C:\Users\andy_chien\Downloads\資料\REPORT.pdf"
+# excel_output_path = r'C:\Users\andy_chien\Downloads\資料\弱層檢核\OUTPUT\report.xlsx'
+# pdf_output_file = r'C:\Users\andy_chien\Downloads\資料\弱層檢核\OUTPUT'
+# final_pdf = r'C:\Users\andy_chien\Downloads\資料\REPORT.pdf'
 
+ui_signal = None
 
+class Text_Color(Enum):
+    black = Qt.black
+    darkYellow = Qt.darkYellow
+    red = Qt.red
+    blue = Qt.blue
+    darkGreen = Qt.darkGreen
 
-def Main():
+class UI_Signal():
+    def __init__(self, ui_signal=None):
+        self.ui_signal = ui_signal
+
+    def show_status(self, txt, color=Text_Color.black.value):
+        if self.ui_signal:
+            self.ui_signal.status.emit((txt, color))
+    
+    def MSG_exception(self, data:str):
+        if self.ui_signal:
+            self.ui_signal.exception.emit(data)
+    
+    def Finished(self):
+        if self.ui_signal:
+            self.ui_signal.finish.emit()
+
+def debug(func):
+    def warpper(*args, **wargs):
+        try:
+            func(*args, **wargs)
+        except Exception as ex:
+            error_class = ex.__class__.__name__ #取得錯誤類型
+            detail = ex.args[0] #取得詳細內容
+            cl, exc, tb = sys.exc_info() #取得Call Stack
+            lastCallStack = traceback.extract_tb(tb)[-1] #取得Call Stack的最後一筆資料
+            fileName = lastCallStack[0] #取得發生的檔案名稱
+            lineNum = lastCallStack[1] #取得發生的行號
+            funcName = lastCallStack[2]#取得發生的函數名稱
+            errMsg = f"{[error_class]}\n\"{fileName}\", line {lineNum}, in {funcName}\n{detail}"
+            print(errMsg)
+            ui_signal.MSG_exception(f'Error: {errMsg}')
+        else:
+            ui_signal.Finished()
+    return warpper
+
+@debug
+def Main(data:dict, signals=None):
+    global ui_signal
+    ui_signal = UI_Signal(signals)
+
+    pdf_list = [os.path.join(os.path.dirname(os.path.abspath(__file__)), "弱層說明頁.pdf")]
+
+    excel_output_path, final_pdf_path = get_output_path(data['input_path'])
     X_shear_data, Y_shear_data = DataTransfer(data)
     Transfer_Excel(X_shear_data, Y_shear_data, excel_output_path)
-    pdf_list = Transfer_PDF(excel_output_path,pdf_output_file)
-    PDF_Merge(pdf_list, final_pdf)
+    pdf_list = Transfer_PDF(pdf_list, excel_output_path, data['input_path'])
+    PDF_Merge(pdf_list, final_pdf_path)
+
+
+def get_output_path(input_path):
+    excel_output_path = os.path.join(input_path, "report.xlsx")
+    final_pdf_path = os.path.join(input_path, "REPORT.pdf")
+    return excel_output_path, final_pdf_path
 
 
 
 
 
 if __name__ == "__main__":
-    Main()
+    Main(data)
--- LFSResult/code/LSF_UI.py
+++ LFSResult/code/LSF_UI.py
@@ -1,26 +1,32 @@
 from glob import glob
-from PySide6.QtWidgets import QApplication, QVBoxLayout, QWidget, QPushButton, QRadioButton, QLabel, QLineEdit, QHBoxLayout, QPlainTextEdit, QFileDialog
-from PySide6.QtCore import Qt
-
+from PySide6.QtWidgets import QApplication, QVBoxLayout, QWidget, QPushButton, QRadioButton, QLabel, QLineEdit, QHBoxLayout, QPlainTextEdit, QFileDialog, QTextBrowser,QMessageBox
+from PySide6.QtGui import QTextCharFormat, QFont, QColor, QTextCursor
+from PySide6.QtCore import Qt, Signal, QRunnable, QObject, QThreadPool, QTimer
+from LSF_Main import Main
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.data = dict()
+        self.start_flag = True
+        self.dot_count = 0
+        self.timer = QTimer()
+        self.threadpool = QThreadPool()
         self.initialize()
         self.showResultSet()
         self.optionBtmSet()
         self.import_btm_connect()
+        self.start_btm_connect()
     
     def initialize(self):
-        self.setWindowTitle("LSF")
+        self.setWindowTitle("LSFResult")
 
         self.generallayout = QVBoxLayout()
         self.option_btm = QRadioButton("校正")
 
         self.input_layout = QHBoxLayout()
         self.label_level = QLabel("Level: ")
-        self.label_H = QLabel("H(m)")
+        self.label_H = QLabel("H(m): ")
         self.level_input = QLineEdit()
         self.H_input = QLineEdit()
 
@@ -36,11 +42,12 @@ class MainWindow(QWidget):
 
         self.import_btm = QPushButton("Import")
         self.start_btm = QPushButton("Start")
+        self.start_btm.setEnabled(False)
         self.btmlayout = QHBoxLayout()
         self.btmlayout.addWidget(self.import_btm)
         self.btmlayout.addWidget(self.start_btm)
-
-        self.show_result = QPlainTextEdit()
+        
+        self.show_result = QTextBrowser()
 
         self.generallayout.addLayout(self.btmlayout)
         self.generallayout.addWidget(self.option_btm)
@@ -49,10 +56,10 @@ class MainWindow(QWidget):
 
         self.setLayout(self.generallayout)
 
+    #region 結果顯示設定
     def showResultSet(self):
-        self.show_result.setLineWrapMode(QPlainTextEdit.NoWrap)
+        self.show_result.setLineWrapMode(QTextBrowser.NoWrap)
         self.show_result.setReadOnly(True)
-
 
     def setDisplayText(self, show_data:tuple):
         txt, color = show_data
@@ -60,7 +67,9 @@ class MainWindow(QWidget):
         fft1.setForeground(color)     
         self.show_result.setCurrentCharFormat(fft1)
         self.show_result.insertPlainText(f"{txt}\n")
+    #endregion
 
+    #region 校正選擇紐設定
     def optionBtmSet(self):
         self.option_btm.toggled.connect(self.hidden)
     
@@ -75,7 +84,9 @@ class MainWindow(QWidget):
             self.label_H.setEnabled(False)
             self.level_input.setEnabled(False)
             self.H_input.setEnabled(False)
+    #endregion
     
+    #region 選擇資料夾設定
     def import_btm_connect(self):
         self.import_btm.clicked.connect(self.select_folder)
 
@@ -84,17 +95,136 @@ class MainWindow(QWidget):
         input_folder_path = QFileDialog.getExistingDirectory(self, 'choose folder', 'F:/Job')
         input_folder_path = input_folder_path.replace("/", "\\")
         self.setDisplayText((f"選擇路徑: {input_folder_path}", Qt.black))
+        self.file_Check(input_folder_path)
+
+    def file_Check(self, input_folder_path):
+        compare_dic={'VPDATXE.txt':'input_X',
+                     'VPDATXE_NSW.txt':'input_X_shear', 
+                     'VPDATYE.txt':'input_Y',
+                     'VPDATYE_NSW.txt':'input_Y_shear'}
+        
+        self.data['input_path'] = input_folder_path
         file_list = glob(f"{input_folder_path}\*.txt")
 
-        self.data['input_path'] = input_folder_path
-        self.data['file_list'] = file_list
-        self.data['file_name_dic'] = {file.split('\\')[-1].split('.')[0]: file for file in file_list}
+        self.setDisplayText((f"內容: ", Qt.black))
         for file in file_list:
-            file = file.split('\\')[-1]
-            self.setDisplayText((f"   {file}", Qt.black))
-        self.setDisplayText((f"總共發現{len(file_list)}個檔案", Qt.darkYellow))
-    
-    
+            file_t = file.split('\\')[-1]
+            self.setDisplayText((f"  {file_t}", Qt.black))
+
+            for key, value in compare_dic.items():
+                if file.endswith(key):
+                    self.data[value] = file
+        
+        if len(file_list) == 4:
+            self.setDisplayText((f"總共發現{len(file_list)}個檔案, 檔案正確\n", Qt.darkYellow))
+            self.start_btm.setEnabled(True)
+        else:
+            self.setDisplayText((f"發現{len(file_list)}個檔案, 有檔案缺少!!", Qt.red))
+            self.Reset_To_Defult()
+    #endregion
+
+    #region 獲得校正狀態與資料
+    def get_correction_data(self):
+        ifcorrection = self.option_btm.isChecked()
+        self.data['adjust'] = ifcorrection
+        if ifcorrection:
+            self.data['level'] = self.level_input.text().strip()
+            self.data['H'] = self.H_input.text().strip()
+            try:
+                self.data['H'] = float(self.data['H'])
+            except:
+                self.setDisplayText((f"H(m)請填入數字!!", Qt.red))
+                self.start_flag = False
+        
+            if self.data['level'] == '':
+                self.setDisplayText((f"Level請填入值!!", Qt.red))
+                self.start_flag = False  
+    #endregion
+
+    #region 開始按鈕設定
+    def start_btm_connect(self):
+        self.start_btm.clicked.connect(self.get_correction_data) 
+        self.start_btm.clicked.connect(self.start_flow)
+    #endregion
+
+    def start_flow(self):
+        if self.start_flag:
+            self.startuiset()
+            self.start_animate()
+            self.start_thread()
+
+    def startuiset(self):
+        self.start_btm.setEnabled(False)
+        self.import_btm.setEnabled(False)
+
+    #region 字串動畫設定
+    def start_animate(self):
+        self.timer.timeout.connect(lambda:self.string_animate('轉換中'))  
+        self.timer.setInterval(500)
+        self.timer.start()
+
+    def string_animate(self, txt):
+        fmt = self.show_result.currentCharFormat()
+        fmt.setForeground(QColor("red"))  
+
+        cursor = self.show_result.textCursor()  
+        cursor.movePosition(cursor.End)
+        cursor.movePosition(cursor.StartOfLine, cursor.KeepAnchor)
+        lastLine = cursor.selectedText()
+
+        if txt in lastLine:
+            cursor.removeSelectedText()
+
+        cursor.insertText(txt + '.' * self.dot_count, fmt)
+        self.dot_count = (self.dot_count + 1) % 4  # 讓'.'在0到3之間循環
+    #endregion
+
+    #region 警告視窗
+    def MSGBox_WARN(self, txt):
+        self.Reset_To_Defult()
+        QMessageBox.warning(self, "Oops", txt)
+        self.setDisplayText((f"已停止", Qt.red)) 
+    #endregion
+
+    #region reset to defult
+    def Reset_To_Defult(self):
+        self.data.clear()
+        self.start_flag = True
+        self.dot_count = 0
+        self.timer.stop()
+        self.start_btm.setEnabled(False)
+        self.import_btm.setEnabled(True)
+    #endregion
+
+    def finish(self):
+        self.setDisplayText(("\n\nDone!", Qt.black))
+        self.Reset_To_Defult()
+        self.timer.stop()
+
+    #region transfer thread
+    def start_thread(self):
+        print(self.data)
+        self.start_thread = start_process(self.data)
+        self.start_thread.signals.status.connect(self.setDisplayText)
+        self.start_thread.signals.exception.connect(self.MSGBox_WARN)
+        self.start_thread.signals.finish.connect(self.finish)
+        self.threadpool.start(self.start_thread)
+    #endregion
+
+
+class ThreadSignal(QObject):
+    status = Signal(tuple)
+    exception = Signal(str)
+    finish = Signal()
+
+class start_process(QRunnable):
+    def __init__(self, data):
+        super(start_process, self).__init__()  
+        self.signals = ThreadSignal()
+        self.data = data
+        
+    def run(self):
+        Main(self.data, self.signals)    
 
 if __name__ == "__main__":
     app = QApplication([])
